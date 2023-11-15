b558608cfb3c46853733ee958c4c37917b71802a
felpo <feliperangel897@gmail.com>
2023-11-15 19:22:22 -0300

    made the divs resize acording to the grid size

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ criar borracha
 criar modo colorido
 colocar um background bonito
 adicionar sombra ao container
-mudar o tamanho da grid de acordo com um input do usuario (máx 64)
+mudar o tamanho da grid de acordo com um input do usuario (máx 100)
 */
 container = document.querySelector('#container')
 
 let isPainting = false
-let grid = 16
+let grid = 100
 
 let i = 0
 while (i < grid * grid) {
@@ -26,5 +26,7 @@ while (i < grid * grid) {
         }
     })
     container.appendChild(div)
+    div.style.width = 400 / grid + "px"
+    div.style.height = 400 / grid + "px"
     i++
 }
