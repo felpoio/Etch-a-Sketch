542632db2fcf3841f546e1a2f10aed60b662deea
felpo <feliperangel897@gmail.com>
2023-11-15 18:40:25 -0300

    add the drawing functionality

--- script.js
+++ script.js
@@ -5,3 +5,19 @@ while (i < 256) {
     container.appendChild(document.createElement('div'))
     i++
 }
+
+let isPainting = false
+divs = document.querySelectorAll("#container > div")
+for (let i = 0; i < divs.length; i++) {
+    divs[i].addEventListener("mousedown", function(){
+        isPainting = true
+    })
+    divs[i].addEventListener("mouseup", function(){
+        isPainting = false
+    })
+    divs[i].addEventListener("mouseenter", function(){
+        if (isPainting){
+            divs[i].style.backgroundColor = "black"
+        }
+    })
+}
