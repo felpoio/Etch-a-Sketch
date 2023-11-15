3066e680bc961fdd88206860363eacc85e595843
felpo <feliperangel897@gmail.com>
2023-11-15 19:08:49 -0300

    organize code for better reading

--- script.js
+++ script.js
@@ -5,28 +5,26 @@ criar modo colorido
 colocar um background bonito
 adicionar sombra ao container
 mudar o tamanho da grid de acordo com um input do usuario (máx 64)
-
 */
 container = document.querySelector('#container')
 
-let i = 0
-while (i < 256) {
-    container.appendChild(document.createElement('div'))
-    i++
-}
-
 let isPainting = false
-divs = document.querySelectorAll("#container > div")
-for (let i = 0; i < divs.length; i++) {
-    divs[i].addEventListener("mousedown", function(){
+let grid = 16
+
+let i = 0
+while (i < grid * grid) {
+    const div = document.createElement('div')
+    div.addEventListener("mousedown", function(){
         isPainting = true
     })
-    divs[i].addEventListener("mouseup", function(){
+    div.addEventListener("mouseup", function(){
         isPainting = false
     })
-    divs[i].addEventListener("mouseenter", function(){
+    div.addEventListener("mouseenter", function(){
         if (isPainting){
-            divs[i].style.backgroundColor = "black"
+            div.style.backgroundColor = "black"
         }
     })
+    container.appendChild(div)
+    i++
 }
